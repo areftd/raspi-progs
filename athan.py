ad934567b9252f77e917e56b42fb6fa3243bbc32
Aref Takiden <aref.takiden@campus.tu-berlin.de>
2017-02-26 14:21:39 +0100

    log to a log-file

--- athan.py
+++ athan.py
@@ -40,31 +40,32 @@ path = '/home/pi/raspi-progs'
 f_name = 'gebetskalendar.csv'
 time_dict = get_times(path, f_name)
 count = 0
-while True:
-    t = datetime.now().isoformat()
-    # print('t = {}'.format(t))
-    _day = get_day(t)
-    if _day in time_dict.keys():
-        running_times = [strptime(tt, "%H:%M") for tt in time_dict[_day]]
-        while len(running_times) != 0:
-            current_clock = datetime.now().isoformat()
-            clock = get_clock(current_clock)
-            print(datetime.now().isoformat())
-            if running_times[0] < clock:
-                running_times.pop(0)
-            elif running_times[0] == clock:
-                if len(running_times) == 4:
-                    print(datetime.now().isoformat())
-                    os.system('mpg123 -f 20000 -o s /home/pi/Music/tathan.mp3')
+with open(os.path.join(path, 'times.log'), 'aw') as ouf:
+    while True:
+        t = datetime.now().isoformat()
+        # print('t = {}'.format(t))
+        _day = get_day(t)
+        if _day in time_dict.keys():
+            running_times = [strptime(tt, "%H:%M") for tt in time_dict[_day]]
+            while len(running_times) != 0:
+                current_clock = datetime.now().isoformat()
+                clock = get_clock(current_clock)
+                ouf.write(datetime.now().isoformat()+'\n')
+                if running_times[0] < clock:
+                    running_times.pop(0)
+                elif running_times[0] == clock:
+                    if len(running_times) == 4:
+                        ouf.write('Athan at '+ datetime.now().isoformat()+'\n')
+                        os.system('mpg123 -f 20000 -o s /home/pi/Music/tathan.mp3')
+                    else:
+                        ouf.write('Athan at '+ datetime.now().isoformat()+'\n')
+                        os.system('mpg123 -f 30000 -o s /home/pi/Music/tathan.mp3')
+                    # print('clock = {}'.format(clock))
+                    running_times.pop(0)
                 else:
-                    print(datetime.now().isoformat())
-                    os.system('mpg123 -f 30000 -o s /home/pi/Music/tathan.mp3')
-                # print('clock = {}'.format(clock))
-                running_times.pop(0)
-            else:
-                ss = datetime.now().isoformat()
-                # print('sleep at ' + ss[11:19])
-                # print('length = {}'.format(len(running_times)))
-                sleep(15)
-    break
+                    ss = datetime.now().isoformat()
+                    # print('sleep at ' + ss[11:19])
+                    # print('length = {}'.format(len(running_times)))
+                    sleep(15)
+        break
 
